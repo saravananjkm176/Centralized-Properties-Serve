89b80ab3288481852de041ade45cf44b71c01b33
saravanan <saravanan.sambandam@photoninfotech.net>
2021-07-24 17:34:37 +0530

    common properties maintaining here

---
+++ src/main/java/com/s3/config/server/main/CentralizedPropertiesServerApplication.java
@@ -0,0 +1,15 @@
+package com.s3.config.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CentralizedPropertiesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedPropertiesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s3/config/server/main/CentralizedPropertiesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3.config.server.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedPropertiesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
